3ddda0859984b3ac3943a305bb2f7c2b82d3b5c7
Marta Łoskot <loskotmarta@gmail.com>
2024-04-16 18:55:57 +0200

    Deleting tasks

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
 const taskContainer = document.querySelector('.task-container')
 
-function render(description) {
-    const taskBlock = document.createElement('div')
-    const title = document.createElement('p')
-    const deleteElement = document.createElement('p')
-    const controller = document.createElement('button')
+let tasks = [
+    {
+        name: "Practise JS sort method",
+        priority: 2
+    },
+    {
+        name: "Adoration !",
+        priority: 1
+    }
+]
+// sort by priority
+const descendingTasks = tasks.sort((a, b) => a.priority - b.priority)
 
-    taskBlock.classList.add('task-block')
-    deleteElement.classList.add('delete-icon')
-    controller.classList.add('controller-button')
-   
-    title.textContent = description
-    deleteElement.textContent = '✖'
-    controller.textContent = 'start'
+function render() {
+    descendingTasks.forEach(task => {
+        const taskBlock = document.createElement('div')
+        const title = document.createElement('p')
+        const deleteElement = document.createElement('p')
+        const controller = document.createElement('button')
 
-    
-    taskBlock.append(deleteElement, title, controller)
-    taskContainer.append(taskBlock)
+        taskBlock.classList.add('task-block')
+        deleteElement.classList.add('delete-icon')
+        controller.classList.add('controller-button')
+
+        title.textContent = task.name
+        deleteElement.textContent = '✖'
+        controller.textContent = 'start'
+
+        deleteElement.addEventListener('click', deleteTask)
+
+        taskBlock.append(deleteElement, title, controller)
+        taskContainer.append(taskBlock)
+    })
 }
 
-render('Cześć!')
+render()
+
+function deleteTask(e) {
+    console.log(e.target.parentNode)
+    e.target.parentNode.remove()
+    // console.log(tasks) to do make disappear from array !
+}
